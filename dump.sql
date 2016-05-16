b76808b2a309994c3595c5097cf17a9220e25d72
Greg Bowler <greg.bowler@g105b.com>
2016-05-16 15:32:59 +0100

    Database stuff for #5

---
+++ dump.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `layout` (
+  `id_layout` int(11) NOT NULL AUTO_INCREMENT,
+  `id_project` int(11) DEFAULT NULL,
+  `order` int(5) NOT NULL DEFAULT '0',
+  `element` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `content` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id_layout`),
+  KEY `fk_layout__project_idx` (`id_project`),
+  CONSTRAINT `fk_layout__project` FOREIGN KEY (`id_project`) REFERENCES `project` (`id_project`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `project` (
+  `id_project` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(256) NOT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id_project`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- notes.md
+++ notes.md
@@ -10,3 +10,13 @@ Summary of work:
 * Iframe used to contain whole page.
 * Twitter Bootstrap elements drawn in iframe.
 * Template HTML used.
+
+Database
+--------
+
+The `dump.sql` file contains the newly improved database. It reflects the original design but now has the following capabilities:
+
+* `Layout` elements store their type as before, but now can store text content for inserting into template elements.
+* `Layout` elements are contained within `Project` elements
+* `Project` elements can be owned by a `User`. More on that later.
+
