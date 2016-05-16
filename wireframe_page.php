5b15a20cc0cf2d089e8e833eab8749174f6f20e2
Greg Bowler <greg.bowler@g105b.com>
2016-05-16 16:37:11 +0100

    Output content into templates

--- wireframe_page.php
+++ wireframe_page.php
@@ -1,8 +1,46 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css" />
+	<link rel="stylesheet" href="css/wireframer-theme.css" />
+	<script src="js/jquery-2.2.4.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+
 <?php
 require_once("db.php");
 require_once("session.php");
 
 $db = new DB();
 foreach($db->getRows() as $row) {
-	// TODO: Output correct template.
+	$templateFile = __DIR__ . "/template/$row[element].html";
+	if(!file_exists($templateFile)) {
+		die("Error: Template not found ($row[element])");
+	}
+
+	$html = file_get_contents($templateFile);
+
+	if(strstr($row["content"], "{")) {
+		$contentArray = explode("{", $row["content"]);
+		foreach ($contentArray as $c) {
+			if(empty($c)) {
+				continue;
+			}
+
+			$cParts = explode("}", $c);
+			$html = preg_replace("/\{\($cParts[0]\).*\}/", $cParts[1], $html);
+		}
+	}
+	else {
+		$html = preg_replace("/\{.*\}/", $row["content"], $html);
+	}
+
+	echo $html;
 }
+?>
+
+</body>
+</html>
