072b13d964a6d21d8ff542eccb937cc9a8349440
Greg Bowler <greg.bowler@g105b.com>
2016-06-07 11:47:46 +0100

    Change order of table

--- dump.sql
+++ dump.sql
@@ -1,3 +1,10 @@
+CREATE TABLE `project` (
+  `id_project` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(256) NOT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id_project`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `layout` (
   `id_layout` int(11) NOT NULL AUTO_INCREMENT,
   `id_project` int(11) DEFAULT NULL,
@@ -8,10 +15,3 @@ CREATE TABLE `layout` (
   KEY `fk_layout__project_idx` (`id_project`),
   CONSTRAINT `fk_layout__project` FOREIGN KEY (`id_project`) REFERENCES `project` (`id_project`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE TABLE `project` (
-  `id_project` int(11) NOT NULL AUTO_INCREMENT,
-  `user` varchar(256) NOT NULL,
-  `name` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id_project`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
