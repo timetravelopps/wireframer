26231a857b9f6c2e86760630b9ad57140575b575
Greg Bowler <greg.bowler@g105b.com>
2016-06-07 11:51:55 +0100

    Add example

---
+++ data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `layout` VALUES (1,1,2,'carousel','{title1}Greg\'s test carousel!{text1}This is coming from the database!{title2}Oh yeah, another page...{text2}I\'ve run out of things to say now.'),(2,1,1,'heading','Welcome to my test project!');
